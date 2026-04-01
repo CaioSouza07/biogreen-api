bf886ef45541fbc1672c58d374c33aa1991b8368
Caio de Souza <caiodesouza.cds@gmail.com>
2026-04-01 10:46:37 -0300

    criado entidade de usuario

---
+++ src/main/java/com/api/biogreen/domain/usuario/Usuario.java
@@ -0,0 +1,26 @@
+package com.api.biogreen.domain.usuario;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "usuarios")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String email;
+    private String senha;
+
+}
