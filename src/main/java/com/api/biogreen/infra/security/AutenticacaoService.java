0f6146b29a5e46adb7d73923f94aecaa18f660e5
Caio de Souza <caiodesouza.cds@gmail.com>
2026-04-01 13:49:18 -0300

    criacao do service para autenticar usuario

---
+++ src/main/java/com/api/biogreen/infra/security/AutenticacaoService.java
@@ -0,0 +1,21 @@
+package com.api.biogreen.infra.security;
+
+import com.api.biogreen.domain.usuario.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AutenticacaoService implements UserDetailsService {
+
+    private final UsuarioRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return repository.findByEmail(email);
+    }
+}
