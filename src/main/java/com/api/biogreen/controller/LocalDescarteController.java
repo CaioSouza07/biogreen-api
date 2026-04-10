934332d34916f21e55049b1efbdea61cdc1e2884
Caio de Souza <caiodesouza.cds@gmail.com>
2026-04-10 11:23:11 -0300

    criado endpoint para salvar, deletar e obter locais de descarte

--- src/main/java/com/api/biogreen/controller/LocalDescarteController.java
+++ src/main/java/com/api/biogreen/controller/LocalDescarteController.java
@@ -4,11 +4,11 @@ import com.api.biogreen.domain.local_descarte.DadosCadastroLocalDescarteDTO;
 import com.api.biogreen.domain.local_descarte.DadosDetalhamentoLocalDescarteDTO;
 import com.api.biogreen.domain.local_descarte.LocalDescarteService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.validation.Valid;
@@ -27,6 +27,24 @@ public class LocalDescarteController {
         var localDescarte = service.cadastrar(dados);
         var uri = uriBuilder.path("localdescarte/{id}").buildAndExpand(localDescarte.getId()).toUri();
         return ResponseEntity.created(uri).body(localDescarte);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<DadosDetalhamentoLocalDescarteDTO> detalhar(@PathVariable Long id){
+        var local = service.detalhar(id);
+        return ResponseEntity.ok(local);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<DadosDetalhamentoLocalDescarteDTO>> listarLocaisDescarte(@PageableDefault(size = 10)Pageable paginacao){
+        var page = service.listarLocalDescarte(paginacao);
+        return ResponseEntity.ok(page);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deletar(@PathVariable Long id){
+        service.deletar(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
--- src/main/java/com/api/biogreen/domain/local_descarte/LocalDescarteService.java
+++ src/main/java/com/api/biogreen/domain/local_descarte/LocalDescarteService.java
@@ -3,7 +3,10 @@ package com.api.biogreen.domain.local_descarte;
 import com.api.biogreen.infra.client.nominatim.NominatimClient;
 import com.api.biogreen.infra.client.viacep.ViaCepClient;
 import com.api.biogreen.infra.exception.CepNaoExistenteException;
+import com.api.biogreen.infra.exception.NotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -23,5 +26,22 @@ public class LocalDescarteService {
         var localDescarte = new LocalDescarte(dados, enderecoCep, coordenadas);
         repository.save(localDescarte);
         return new DadosDetalhamentoLocalDescarteDTO(localDescarte);
+    }
+
+    public DadosDetalhamentoLocalDescarteDTO detalhar(Long id){
+        var localDescarte = repository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Local de Descarte não encontrado"));
+        return new DadosDetalhamentoLocalDescarteDTO(localDescarte);
+    }
+
+    public Page<DadosDetalhamentoLocalDescarteDTO> listarLocalDescarte(Pageable paginacao){
+        return repository.findAll(paginacao).map(DadosDetalhamentoLocalDescarteDTO::new);
+    }
+
+    @Transactional
+    public void deletar(Long id){
+        var localDescarte = repository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Local de Descarte não encontrado"));
+        repository.delete(localDescarte);
     }
 }
